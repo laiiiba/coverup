188eba5fd394b16a940be95d26d08d9830a368bc
Laiba Asif <laiba.asif.blue@gmail.com>
2026-03-06 15:15:29 +0000

    add codecarbon for segmentation, store different codecarbon sections in different files

--- src/coverup/segment.py
+++ src/coverup/segment.py
@@ -71,83 +71,91 @@ def get_missing_coverage(coverage, line_limit: int = 100) -> T.List[CodeSegment]
     """Processes a JSON SlipCover output and generates a list of Python code segments,
     such as functions or classes, which have less than 100% coverage.
     """
+    segmentation_codecarbon = EmissionsTracker(project_name = 'coverup', experiment_id = 'segmentation', output_dir = 'segmentation_codecarbon_logs', log_level = 'error')
+    segmentation_codecarbon.start()
 
-    code_segs = []
+    try: 
+        code_segs = []
 
-    def find_first_line(node):
-        # skip back to include decorators, as they are really part of the definition
-        return min([node.lineno] + [d.lineno for d in node.decorator_list])
+        def find_first_line(node):
+            # skip back to include decorators, as they are really part of the definition
+            return min([node.lineno] + [d.lineno for d in node.decorator_list])
 
-    def find_enclosing(root, line):
-        for node in ast.walk(root):
-            if node is root:
-                continue
-
-            if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef)) and hasattr(node, "lineno"):
-                begin = find_first_line(node)
-                if begin <= line <= node.end_lineno:
-                    return (node, begin, node.end_lineno+1) # +1 for range() style
-
-    for fname, fcov in coverage['files'].items():
-        tree = parse_file(Path(fname))
-
-        missing_lines = set(fcov['missing_lines'])
-        executed_lines = set(fcov['executed_lines'])
-        missing_branches = fcov.get('missing_branches', set())
-
-        line_ranges = dict()
-
-        lines_of_interest = missing_lines.union(set(sum(missing_branches,[])))
-        lines_of_interest.discard(0)  # may result from N->0 branches
-
-        # TODO remove from missing lines with load-time statements?
-        #   - directly under ModuleDef, ClassDef
-        # note that docstring don't generate code, so can't be missing within functions
-        # But then: how to capture missing coverage if a module was never loaded?
-
-        lines_in_segments = set()
-
-        for line in sorted(lines_of_interest):   # sorted() simplifies tests
-            if line in lines_in_segments:
-                # already in a segment
-                continue
-
-            # FIXME add segments for top-level elements (under ModuleDef)
-            if element := find_enclosing(tree, line):
-                node, begin, end = element
-                context = []
-
-                # If a class is above the line limit, look for enclosing element
-                # that might allow us to obey the limit
-                while isinstance(node, ast.ClassDef) and end - begin > line_limit and \
-                      (element := find_enclosing(node, line)):
-                    context.append((begin, node.lineno+1)) # +1 for range() style
-                    node, begin, end = element
-
-                # Don't create a segment for a class that's too large... if we did, we
-                # might create a segment for a class after creating segments for its contents.
-                if isinstance(node, ast.ClassDef) and end - begin > line_limit:
+        def find_enclosing(root, line):
+            for node in ast.walk(root):
+                if node is root:
                     continue
 
-                assert line < end
-                assert (begin, end) not in line_ranges
+                if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef)) and hasattr(node, "lineno"):
+                    begin = find_first_line(node)
+                    if begin <= line <= node.end_lineno:
+                        return (node, begin, node.end_lineno+1) # +1 for range() style
 
-                line_ranges[(begin, end)] = (
-                    node, context, [ast.unparse(x) for x in get_global_imports(tree, node)]
-                )
-                lines_in_segments.update({*range(begin, end)})
+        for fname, fcov in coverage['files'].items():
+            tree = parse_file(Path(fname))
 
-        if line_ranges:
-            for (begin, end), (node, context, imports) in line_ranges.items():
-                line_range_set = {*range(begin, end)}
-                code_segs.append(CodeSegment(
-                    fname, node.name, begin, end,
-                    lines_of_interest=lines_of_interest.intersection(line_range_set),
-                    missing_lines=missing_lines.intersection(line_range_set),
-                    executed_lines=executed_lines.intersection(line_range_set),
-                    missing_branches={tuple(b) for b in missing_branches if b[0] in line_range_set},
-                    context=context,
-                    imports=imports)
-                )
+            missing_lines = set(fcov['missing_lines'])
+            executed_lines = set(fcov['executed_lines'])
+            missing_branches = fcov.get('missing_branches', set())
+
+            line_ranges = dict()
+
+            lines_of_interest = missing_lines.union(set(sum(missing_branches,[])))
+            lines_of_interest.discard(0)  # may result from N->0 branches
+
+            # TODO remove from missing lines with load-time statements?
+            #   - directly under ModuleDef, ClassDef
+            # note that docstring don't generate code, so can't be missing within functions
+            # But then: how to capture missing coverage if a module was never loaded?
+
+            lines_in_segments = set()
+
+            for line in sorted(lines_of_interest):   # sorted() simplifies tests
+                if line in lines_in_segments:
+                    # already in a segment
+                    continue
+
+                # FIXME add segments for top-level elements (under ModuleDef)
+                if element := find_enclosing(tree, line):
+                    node, begin, end = element
+                    context = []
+
+                    # If a class is above the line limit, look for enclosing element
+                    # that might allow us to obey the limit
+                    while isinstance(node, ast.ClassDef) and end - begin > line_limit and \
+                           (element := find_enclosing(node, line)):
+                        context.append((begin, node.lineno+1)) # +1 for range() style
+                        node, begin, end = element
+
+                    # Don't create a segment for a class that's too large... if we did, we
+                    # might create a segment for a class after creating segments for its contents.
+                    if isinstance(node, ast.ClassDef) and end - begin > line_limit:
+                        continue
+
+                    assert line < end
+                    assert (begin, end) not in line_ranges
+
+                    line_ranges[(begin, end)] = (
+                        node, context, [ast.unparse(x) for x in get_global_imports(tree, node)]
+                    )
+                    lines_in_segments.update({*range(begin, end)})
+
+            if line_ranges:
+                for (begin, end), (node, context, imports) in line_ranges.items():
+                    line_range_set = {*range(begin, end)}
+                    code_segs.append(CodeSegment(
+                        fname, node.name, begin, end,
+                        lines_of_interest=lines_of_interest.intersection(line_range_set),
+                        missing_lines=missing_lines.intersection(line_range_set),
+                        executed_lines=executed_lines.intersection(line_range_set),
+                        missing_branches={tuple(b) for b in missing_branches if b[0] in line_range_set},
+                        context=context,
+                        imports=imports)
+                    )
+
+    finally:
+        emissions = segmentation_codecarbon.stop()
+        print(f"\nTotal CO2 emissions for segmentation: {emissions} kg", flush=True)
 
     return code_segs
+
--- src/coverup/testrunner.py
+++ src/coverup/testrunner.py
@@ -44,7 +44,7 @@ def measure_suite_coverage(*, tests_dir: Path, source_dir: T.Optional[Path], pyt
                            trace=None, isolate_tests=False, branch_coverage=True):
     """Runs an entire test suite and returns the coverage obtained."""
 
-    suite_coverage_codecarbon = EmissionsTracker(project_name = 'coverup', experiment_id = 'suite_coverage', output_dir = 'codecarbon_logs', log_level = 'error')
+    suite_coverage_codecarbon = EmissionsTracker(project_name = 'coverup', experiment_id = 'suite_coverage', output_dir = 'suite_coverage_codecarbon_logs', log_level = 'error')
     '''add a more specific label to be able to differentiate different projects here'''
     suite_coverage_codecarbon.start()
     
