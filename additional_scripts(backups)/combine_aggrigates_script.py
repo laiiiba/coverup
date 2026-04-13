72cab1a7ff8c56d9be150613b7a14fbf234a9386
Laiba Asif <laiba.asif.blue@gmail.com>
2026-04-13 20:04:24 +0100

    add complexities to final csv script

--- additional_scripts(backups)/combine_aggrigates_script.py
+++ additional_scripts(backups)/combine_aggrigates_script.py
@@ -7,4 +7,18 @@ data = [pd.read_csv(f) for f in files]
 
 combined = pd.concat(data, ignore_index = True)
 
+complexity = pd.read_csv("../cm-dataset-setup/cm_modules_complexity_results.csv")
+
+combined = combined.merge(
+    complexity[[
+        "project_name",
+        "max_complexity_number",
+        "radon_rank",
+        "complexity_level"
+    ]],
+    on="project_name",
+    how="left"
+)
+
 combined.to_csv("../experiment-results/all_experiments.csv", index=False)
+
